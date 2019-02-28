8fa39185103186652cd8a9452b91fcf422017c68
Ahmad <Ahmad7.974@gmail.com>
2019-02-28 01:16:00 +0100

    init

---
+++ app/entry.js
@@ -0,0 +1,17 @@
+/*
+    1. create a function named `sum` that calculate input parameter with that form:
+    '' -> return 0
+    '1' -> return 1
+    '3,5' -> return 8
+    '2,5,3' -> return 10
+
+    2. allow other separator than `,` user '#' ex:
+    '3#5' -> return 8
+    '2#5#3' -> return 10
+ */
+
+
+
+
+
+module.exports = sum;
---
+++ app/entrySpec.js
@@ -0,0 +1,12 @@
+const sum = require('./entry');
+
+describe('add function test suits', function () {
+
+    it('should return 0', function () {
+
+    });
+
+    it('should return xx', function () {
+
+    });
+});
